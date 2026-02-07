2e0c4a900031ab7bb32e30495d3757ac9aac4531
chulakimath <santoshchulakimath222@gmail.com>
2026-02-08 00:01:50 +0530

    Added raw file view

--- app/routes/views.routes.js
+++ app/routes/views.routes.js
@@ -2,6 +2,27 @@ import express from "express";
 import postsModel from "../models/posts.model.js";
 const router = express.Router();
 
+router.get("/raw/:key", async (req, res) => {
+    const { key } = req.params;
+
+    if (!key) {
+        return res.redirect("/");
+    }
+    try {
+        const response = await postsModel.getByid(key);
+
+        if (!response || response.length === 0) {
+            return res.render("index", { message: "No such post" });
+        }
+
+   
+        res.set("Content-Type", "text/plain; charset=utf-8");
+        res.send(response[0]?.body);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
+});
 router.get("/:key", async (req, res) => {
     const { key } = req.params;
 
@@ -21,6 +42,8 @@ router.get("/:key", async (req, res) => {
         res.status(500).send("Server Error");
     }
 });
+
+
 
 router.get("/", async (req, res) => {
     return res.render("index");
